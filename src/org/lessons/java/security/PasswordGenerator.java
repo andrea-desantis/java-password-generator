a1420e9f1e7b48e6089ffab062b8bf50a3fb621e
andrea-desantis <andrea.desantis290@gmail.com>
2026-03-19 20:59:41 +0100

    first commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package org.lessons.java.security;
+
+
+// - Salvare in opportune variabili il nome, cognome, 
+//  colore preferito e data di nascita di un utente suddivisa 
+//  in giorno, mese e anno in numero
+// - Generare (e stampare a video) una password concatenando nome, 
+// cognome, colore preferito e somma di giorno, mese e 
+// anno di nascita, separate dal carattere -
+
+
+public class PasswordGenerator {
+     public static void main(String[] args) {
+         String nome;
+         String cognome;
+         String colore;
+         int giorno;
+         int mese;
+         int anno;
+         String password;
+    }
+}

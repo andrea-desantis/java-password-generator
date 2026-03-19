283e0922c11046345a0fd2a903d20720ac2585af
andrea-desantis <andrea.desantis290@gmail.com>
2026-03-19 21:09:21 +0100

    feat: password

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,6 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
 
 // - Salvare in opportune variabili il nome, cognome, 
 //  colore preferito e data di nascita di un utente suddivisa 
@@ -11,12 +12,35 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
      public static void main(String[] args) {
-         String nome;
-         String cognome;
-         String colore;
-         int giorno;
-         int mese;
-         int anno;
-         String password;
+        String nome;
+        String cognome;
+        String colore;
+        int giorno;
+        int mese;
+        int anno;
+        String password;
+
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Inserire Nome: ");
+        nome = in.nextLine();
+
+        System.out.println("Inserire Cognome: ");
+        cognome = in.nextLine();
+
+        System.out.println("Inserire Colore Preferito: ");
+        colore = in.nextLine();
+
+        System.out.println("Inserire Anno di Nascita: ");
+        anno = in.nextInt();
+
+        System.out.println("Inserire Mese di Nascita: ");
+        mese = in.nextInt();
+
+        System.out.println("Inserire Numero del Mese di Nascita: ");
+        giorno = in.nextInt();
+
+        password = nome + "-" + cognome + "-" + colore + (giorno + mese + anno);
+        System.out.println("Ecco a lei la sua password -> " + password);
     }
 }
